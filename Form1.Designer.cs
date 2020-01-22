dbac76937bceeb8effec8e0d4747c29a3535c73d
Freddie Fellowes <ffellowes@hotmail.co.uk>
2020-01-22 15:55:16 +0000

    basic-1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ms = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.bClearRTB = new System.Windows.Forms.Button();
+            this.ms.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ms
+            // 
+            this.ms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.ms.Location = new System.Drawing.Point(0, 0);
+            this.ms.Name = "ms";
+            this.ms.Size = new System.Drawing.Size(393, 24);
+            this.ms.TabIndex = 0;
+            this.ms.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(12, 85);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(369, 232);
+            this.rtb.TabIndex = 1;
+            this.rtb.Text = "";
+            this.rtb.TextChanged += new System.EventHandler(this.rtb_TextChanged);
+            this.rtb.KeyDown += new System.Windows.Forms.KeyEventHandler(this.rtb_KeyDown);
+            // 
+            // bClearRTB
+            // 
+            this.bClearRTB.Location = new System.Drawing.Point(12, 323);
+            this.bClearRTB.Name = "bClearRTB";
+            this.bClearRTB.Size = new System.Drawing.Size(75, 23);
+            this.bClearRTB.TabIndex = 2;
+            this.bClearRTB.Text = "Clear RTB";
+            this.bClearRTB.UseVisualStyleBackColor = true;
+            this.bClearRTB.Click += new System.EventHandler(this.bClearRTB_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 460);
+            this.ClientSize = new System.Drawing.Size(393, 434);
+            this.Controls.Add(this.bClearRTB);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.ms);
+            this.MainMenuStrip = this.ms;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.ms.ResumeLayout(false);
+            this.ms.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip ms;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button bClearRTB;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,68 @@ namespace cd_WinForms_3
         public Form1()
         {
             InitializeComponent();
+
+            this.Text = "C# Messing";
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            rtb.Clear();
+            rtb.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
+            // to force scrolling to end of added text, use the TextChanged() method
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd.FileName = string.Empty;
+            ofd.Title = "Open Text File";
+            //ofd.Filter = "*.txt|(*.txt)|*.*|(*.*)";
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            ofd.FilterIndex = 0;
+
+            if (ofd.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            // display contents of the file in the text box
+            if (ofd.CheckFileExists == false)
+                rtb.Text += "*** Specified file doesn't exist";
+            else
+                rtb.Text += File.ReadAllText(ofd.FileName);
+        }
+
+        private void bClearRTB_Click(object sender, EventArgs e)
+        {
+            rtb.Clear();
+        }
+
+        private void rtb_KeyDown(object sender, KeyEventArgs e)
+        {
+            // trap Ctrl-A but don't select all text
+            if (e.Control && e.KeyCode.ToString() == "A")
+            {
+                rtb.Text += ">>> trapped Ctrl-A";
+
+                e.Handled = true;
+            }
+
+            // if Ctrl-Alt-A pressed then select all text
+            if( e.Control && e.Alt && e.KeyCode.ToString() == "A")
+            {
+                rtb.Text += ">>> Ctrl-Alt-A trapped";
+                rtb.SelectAll();
+                e.Handled = true;
+            }
+        }
+
+        private void rtb_TextChanged(object sender, EventArgs e)
+        {
+            rtb.SelectionStart = rtb.Text.Length;
+            // scroll it automatically
+            rtb.ScrollToCaret();
         }
     }
 }
